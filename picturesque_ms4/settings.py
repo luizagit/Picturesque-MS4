229832c7efdff30cd35ce5e457e0da2a1c3cef86
luizagit <caraimanluiza@gmail.com>
2020-10-06 20:30:32 +0100

    created secrets.py to store passwords, added it to .gitignore and imported into settings.py

--- picturesque_ms4/settings.py
+++ picturesque_ms4/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from secrets import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,8 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '8j!of&2j8327w7c-4amzr3^th6&aqm**3&2d2f#-%0=1-&_d*#'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
