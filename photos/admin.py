be8fd44e838fbbba0a565c3bd9f6f8352c1d7622
luizagit <caraimanluiza@gmail.com>
2020-10-12 19:34:20 +0100

    added customization to admin page

--- photos/admin.py
+++ photos/admin.py
@@ -2,5 +2,24 @@ from django.contrib import admin
 from .models import Photo, Category
 
 # Register your models here.
-admin.site.register(Photo)
-admin.site.register(Category)
+
+class PhotoAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'rating',
+        'image',
+    )
+
+    ordering = ('sku',)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+admin.site.register(Photo, PhotoAdmin)
+admin.site.register(Category, CategoryAdmin)
