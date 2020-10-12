7ca2ef2119c181acca76a5db766ab94c3c588525
luizagit <caraimanluiza@gmail.com>
2020-10-12 22:14:58 +0100

    created all_photos view in photos app

--- photos/views.py
+++ photos/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Photo
 
 # Create your views here.
+
+def all_photos(request):
+    """ A view to show all photos, with sorting feature included"""
+
+    photos = Photo.objects.all()
+
+    context = {
+        'photos': photos,
+    }
+
+    return render(request, 'photos/photos.html', context)
