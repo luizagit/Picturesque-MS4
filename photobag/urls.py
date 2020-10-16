41352bb05d47fcce69691add86c71da0b5caf13b
luizagit <caraimanluiza@gmail.com>
2020-10-16 22:46:05 +0100

    added add_to_photobag view to urls.py

--- photobag/urls.py
+++ photobag/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.view_photobag, name='view_photobag')
+    path('', views.view_photobag, name='view_photobag'),
+    path('add/<item_id>/', views.add_to_photobag, name='add_to_photobag'),
 ]
