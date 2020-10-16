9321089be5693c6e6b5743f5bf12f65ff275bab5
luizagit <caraimanluiza@gmail.com>
2020-10-16 22:45:06 +0100

    added view add_to_photobag

--- photobag/views.py
+++ photobag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
@@ -6,3 +6,19 @@ def view_photobag(request):
     """ A view that renders the bag contents page """
 
     return render(request, 'photobag/photobag.html')
+
+
+def add_to_photobag(request, item_id):
+    """ Add a quantity of the specified product to the photoshopping bag """
+
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    photobag = request.session.get('photobag', {})
+
+    if item_id in list(photobag.keys()):
+        photobag[item_id] += quantity
+    else:
+        photobag[item_id] = quantity
+
+    request.session['photobag'] = photobag
+    return redirect(redirect_url)
