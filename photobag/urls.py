3ee417930d2d085f980124ad5243f7c873ca871d
luizagit <caraimanluiza@gmail.com>
2020-10-17 22:02:51 +0100

    added adjust_photobag view to urls

--- photobag/urls.py
+++ photobag/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.view_photobag, name='view_photobag'),
     path('add/<item_id>/', views.add_to_photobag, name='add_to_photobag'),
+    path('adjust/<item_id>/', views.adjust_photobag, name ='adjust_photobag'),
 ]
