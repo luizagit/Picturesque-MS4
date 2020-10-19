62f8385c6928adcf99a430a828878bd630f1032f
luizagit <caraimanluiza@gmail.com>
2020-10-19 21:50:24 +0100

    removed secret key and set debug

--- picturesque_ms4/settings.py
+++ picturesque_ms4/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '8j!of&2j8327w7c-4amzr3^th6&aqm**3&2d2f#-%0=1-&_d*#'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['*']
 
