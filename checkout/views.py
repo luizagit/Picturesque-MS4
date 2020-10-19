61cf278ddb40e65d1b7f2d4eb1bc59c19c3f9be7
luizagit <caraimanluiza@gmail.com>
2020-10-19 15:45:52 +0100

    handling in checkout view the creation of order in the database and redirection to a success page

--- checkout/views.py
+++ checkout/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.conf import settings
 
 from .forms import OrderForm
+from .models import Order, OrderLineItem
+from products.models import Photo
 from photobag.contexts import photobag_contents
 
 import stripe
@@ -13,21 +15,64 @@ def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
 
-    photobag = request.session.get('photobag', {})
-    if not photobag:
-        messages.error(request, "There's nothing in your photobag at the moment")
-        return redirect(reverse('photos'))
+    if request.method == 'POST':
+        photobag = request.session.get('photobag', {})
 
-    current_photobag = photobag_contents(request)
-    total = current_photobag['grand_total']
-    stripe_total = round(total * 100)
-    stripe.api_key = stripe_secret_key
-    intent = stripe.PaymentIntent.create(
-        amount=stripe_total,
-        currency=settings.STRIPE_CURRENCY,
-    )
+        form_data = {
+            'full_name': request.POST['full_name'],
+            'email': request.POST['email'],
+            'phone_number': request.POST['phone_number'],
+            'country': request.POST['country'],
+            'postcode': request.POST['postcode'],
+            'town_or_city': request.POST['town_or_city'],
+            'street_address1': request.POST['street_address1'],
+            'street_address2': request.POST['street_address2'],
+            'county': request.POST['county'],
+        }
+        order_form = OrderForm(form_data)
+        if order_form.is_valid():
+            order = order_form.save()
+            for item_id, item_data in photobag.items():
+                try:
+                    photo = Photo.objects.get(id=item_id)
+                    if isinstance(item_data, int):
+                        order_line_item = OrderLineItem(
+                            order=order,
+                            photo=photo,
+                            quantity=item_data,
+                        )
+                        order_line_item.save()
+                except Photo.DoesNotExist:
+                    messages.error(request, (
+                        "One of the photos in your photobag does not exist in our database. "
+                        "Please call us for assistance!")
+                    )
+                    order.delete()
+                    return redirect(reverse('view_photobag'))
 
-    order_form = OrderForm()
+            request.session['save_info'] = 'save-info' in request.POST
+            return redirect(reverse('checkout_success', args=[order.order_number]))
+        else:
+            messages.error(request, 'There was an error with your form. \
+                Please double check your information.')
+
+
+    else:
+        photobag = request.session.get('photobag', {})
+        if not photobag:
+            messages.error(request, "There's nothing in your photobag at the moment")
+            return redirect(reverse('photos'))
+
+        current_photobag = photobag_contents(request)
+        total = current_photobag['grand_total']
+        stripe_total = round(total * 100)
+        stripe.api_key = stripe_secret_key
+        intent = stripe.PaymentIntent.create(
+            amount=stripe_total,
+            currency=settings.STRIPE_CURRENCY,
+        )
+
+        order_form = OrderForm()
 
     if not stripe_public_key:
         messages.warning(request, """Stripe public key is missing, 
@@ -38,6 +83,26 @@ def checkout(request):
         'order_form': order_form,
         'stripe_public_key': stripe_public_key,
         'client_secret': intent.client_secret,
+    }
+
+    return render(request, template, context)
+
+def checkout_success(request, order_number):
+    """
+    Handle successful checkouts
+    """
+    save_info = request.session.get('save_info')
+    order = get_object_or_404(Order, order_number=order_number)
+    messages.success(request, f'Order successfully processed! \
+        Your order number is {order_number}. A confirmation \
+        email will be sent to {order.email}.')
+
+    if 'photobag' in request.session:
+        del request.session['photobag']
+
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order': order,
     }
 
     return render(request, template, context)
