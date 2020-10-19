626a87224b5e60326da44ddcca3604e1eb774a1a
luizagit <caraimanluiza@gmail.com>
2020-10-19 12:28:30 +0100

    added checkout view

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
+
+from .forms import OrderForm
 
 # Create your views here.
+
+def checkout(request):
+    photobag = request.session.get('photobag', {})
+    if not photobag:
+        messages.error(request, "There's nothing in your photobag at the moment")
+        return redirect(reverse('photos'))
+
+    order_form = OrderForm()
+    template = 'checkout/checkout.html'
+    context = {
+        'order_form': order_form,
+    }
+
+    return render(request, template, context)
