88b702222e7588b65714fca34920c91f33eb7698
luizagit <caraimanluiza@gmail.com>
2020-10-19 12:33:13 +0100

    added checkout to urls

--- picturesque_ms4/urls.py
+++ picturesque_ms4/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('', include('home.urls')),
     path('photos/', include('photos.urls')),
     path('photobag/', include('photobag.urls')),
+    path('checkout/', include('checkout.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
