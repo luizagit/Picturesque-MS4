4eaf006c4b8ff616f67abeb2119b6bab34e62918
luizagit <caraimanluiza@gmail.com>
2020-10-19 12:31:47 +0100

    added crispy_forms to settings.py

--- picturesque_ms4/settings.py
+++ picturesque_ms4/settings.py
@@ -46,6 +46,9 @@ INSTALLED_APPS = [
     'photos',
     'photobag',
     'checkout',
+
+    # Other Apps
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -59,6 +62,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'picturesque_ms4.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -74,8 +79,13 @@ TEMPLATES = [
                 'django.template.context_processors.request', # allauth requirement
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'photobag.contexts.photobag_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
