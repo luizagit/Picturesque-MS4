9fb7cd299d8350e15970b913d7d8c3aed1dc8b48
luizagit <caraimanluiza@gmail.com>
2020-10-20 19:16:04 +0100

    updated user_profile and base templates and tested profiles work

--- profiles/views.py
+++ profiles/views.py
@@ -1,11 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from .models import UserProfile
 
 # Create your views here.
 
 def user_profile(request):
     """ Display user profile. """
+    user_profile = get_object_or_404(UserProfile, user=request.user)
 
     template = 'profiles/user_profile.html'
-    context = {}
+    context = {
+        'user_profile': user_profile,
+    }
 
     return render(request, template, context)
