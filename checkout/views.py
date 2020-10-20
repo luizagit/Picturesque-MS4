ab34f5deba6cf91b6ffcd107cae6aa1241deb1ca
luizagit <caraimanluiza@gmail.com>
2020-10-20 11:38:41 +0100

    completed webhook handler features

--- checkout/views.py
+++ checkout/views.py
@@ -49,7 +49,11 @@ def checkout(request):
         }
         order_form = OrderForm(form_data)
         if order_form.is_valid():
-            order = order_form.save()
+            order = order_form.save(commit=False)
+            pid = request.POST.get('client_secret').split('_secret')[0]
+            order.stripe_pid = pid
+            order.original_photobag = json.dumps(photobag)
+            order.save()
             for item_id, item_data in photobag.items():
                 try:
                     photo = Photo.objects.get(id=item_id)
