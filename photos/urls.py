63a71c58a236cb328fc35e3205ea57ce384587ef
luizagit <caraimanluiza@gmail.com>
2020-10-21 16:24:48 +0100

    added edit_photo view to urls.py

--- photos/urls.py
+++ photos/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.all_photos, name='photos'),
     path('<int:photo_id>/', views.photo_detail, name='photo_detail'),
     path('add/', views.add_photo, name='add_photo'),
+    path('edit/<int:photo_id>/', views.edit_photo, name='edit_photo'),
 ]
